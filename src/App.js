f2f25766d0f3d2f5b01b20b8b7eb1786c9f9eb04
ocbilge <ocbilge@mikrokom.com>
2021-08-01 15:22:42 +0300

    create orders

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import React from "react";
-import Products from "./components/Products";
-import Filter from "./components/Filter";
-import Cart from "./components/Cart";
 import { Provider } from "react-redux";
 import store from "./store";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import AdminScreen from "./screens/AdminScreen";
 
 class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="grid-container">
-          <header>
-            <a href="/"> React Shopping Cart</a>
-          </header>
-          <main>
-            <div className="content">
-              <div className="main">
-                <Filter />
-                <Products />
-              </div>
-              <div className="sidebar">
-                <Cart />
-              </div>
-            </div>
-          </main>
-          <footer>All right reserved.</footer>
-        </div>
+        <Router>
+          <div className="grid-container">
+            <header>
+              <Link to="/">React Shopping Cart</Link>
+              <Link to="/admin">Admin</Link>
+            </header>
+            <main>
+              <Route path="/admin" component={AdminScreen} />
+              <Route path="/" component={HomeScreen} exact />
+            </main>
+            <footer>All right reserved.</footer>
+          </div>
+        </Router>
       </Provider>
     );
   }
